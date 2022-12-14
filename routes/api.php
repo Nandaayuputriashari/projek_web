c264d5b6f52183ae6f276ca561342469509dc828
Arilex <110915525+Xaisar@users.noreply.github.com>
2022-12-14 07:22:21 +0700

    api login dan register

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('register', 'App\Http\Controllers\Api\Auth\AuthController@register');
+Route::post('login', 'App\Http\Controllers\Api\Auth\AuthController@login');
